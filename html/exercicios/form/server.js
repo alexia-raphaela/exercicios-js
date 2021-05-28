e860f3bf59bfe7ba42b060676e2faef820f35a77
Aléxia Raphaela <alexiaraphaela@gmail.com>
2021-05-28 11:58:03 -0300

    foi incluido mais um app.post para finalizar a inclusão do botão alterar

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -8,8 +8,20 @@ app.use(bodyParser.urlencoded({ extend: true }))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
+    //requisição do tipo get, od dados não vem no body e sim na query
+    console.log(req.query)
     // Quando o usuario clicar no formulario para submeter
-    resp.send(`<h1>Parabéns</h1>`)
+    resp.send(`<h1>Parabéns. Usuário Incluido!</h1>`)
 })
+
+app.post('/usuarios/:id', (req, resp) => {
+    //mostrar tanto o id que foi obtido a partir dos parametros da url
+    console.log(req.params.id)
+    // obter tudo que veio no body
+    console.log(req.body)
+    // Quando o usuario clicar no formulario para submeter
+    resp.send(`<h1>Parabéns. Usuário Alterado!</h1>`)
+})
+
 
 app.listen(3003)
