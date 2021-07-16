e7b48df6a97e11a24b8db4a6d406b0636b3e6697
Aléxia Raphaela <alexiaraphaela@gmail.com>
2021-07-16 11:35:13 -0300

    novo arquivo zip do capitulo de ajax

---
+++ ajax/ajax/server.js
@@ -0,0 +1,15 @@
+// vai ser o responsavel por ter o servidor tanto para prover os dados estaticos, como tambem para ter alguns serviços
+// a dependencia body parser é responsavel por fazer o parser da requisicao
+// nodemon ira servir para monitorar os arquivos sempre que houver uma mudança
+const bodyParser = require('body-parser')
+const express = require('express')
+// instanciar o express
+const app = express()
+// middleware é uma camada de mediacao entre tecnologias que nao sabem se comunicar ent//re si
+// middleware vai ser executado quando uma determinada requisicao chegar
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/teste', (req, res) => res.send('Ok'))
+app.listen(8080, () => console.log('Executando...'))
