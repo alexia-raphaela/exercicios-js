1c8df60ee727976d89c34745b3ef44d4899116dd
Aléxia Raphaela <alexiaraphaela@gmail.com>
2021-07-20 12:24:55 -0300

    impplementação do conteudo de upload com ajax

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -11,5 +11,31 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
+// fazendo upload de um arquivo
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  //este objeto serve para cofigurar personalizar uma pasta, arquivo 
+  destination: function (req, file, callback) {
+    callback(null, './unpload')
+  },
+  filename: function (req, file, callback) {
+    // quando for escolhido o nome de um arquivo, sera seguido esse padrao de nome
+    callback(null, `${Date.now()}_${file.originalname}`)
+  }
+  
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+  upload(req, res, err => {
+      if (err) {
+        return res.end('Ocorreu um erro.')
+      }
+      res.end('Concluido com sucesso')
+  })
+})
+
+// app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Executando...'))
