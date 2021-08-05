cfef564ed3ad05168bf88b3949cdf2c519154a3a
Alexia Raphaela <alexiaraphaela@gmail.com>
2021-08-05 10:54:49 -0300

    implementando servidor.js

--- gulp/spa/gulpTasks/servidor.js
+++ gulp/spa/gulpTasks/servidor.js
@@ -1,14 +1,29 @@
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+// watch sera utlizado para monitorar os arquivos
+const watch = require('gulp-watch')
 
-function monitorarArquivos(cb) {
-    return cb()
+function servidor() {
+    return gulp.src('build')
+    // passando as configurações do servidor
+        .pipe(webserver({
+            port: 8080,
+            // essa configuração faz com que o seja aberto o browser
+            open: true,
+            livereload: true,
+        }))
 }
 
-function servidor(cb) {
+function monitorarArquivos(cb) {
+    // se esse arquivo mudar sera chamada uma função
+    // e essa função ira chamar uma tesk do gulp
+   watch('src/**/*.html', () => gulp.series('appHTML')())
+   
     return cb()
 }
 
 module.exports = {
     monitorarArquivos,
     servidor
+
 }
