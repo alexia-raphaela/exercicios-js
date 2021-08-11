570174f36a6a99e08556cf21da9433a4fcbc21b6
Alexia Raphaela <alexiaraphaela@gmail.com>
2021-08-11 11:21:13 -0300

    pratica e configuracoes de arquivos, a partir de webpack

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,6 +1,8 @@
 // importando da forma padrao do ecmaScript
 import Pessoa from './pessoa'
 // const Pessoa = require('./pessoa')
+import './modulos/moduloA'
+import './assets/css/estilo.css'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,38 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     // modo de desenvolvimento 
     mode: 'development',
     // ponto de entrada
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        // pasta destino para salvar os arquivos gerados
+        path: __dirname + '/public'
+    },
+    plugins: [
+        // função construtora sendo instanciada, passando objetos como parametros
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {
+        // regras, onde são colocados todos os loders
+        rules: [{
+            //primeiro loder, responsavel por ler arquivos com a extensão .css
+            test: /\.css$/,
+            // verificar se o arquivo foi lido ou não
+            use: [
+                // plugin que extrai para fora o css, para um arquivo externo
+                MiniCssExtractPlugin.loader,
+                // style-loader ira criar uma tag style via dom
+               // 'style-loader', // Adciona CSS a DOM injetando a tag <style>
+                'css-loader' // interpreta @import, url()...
+            ]
+            // ele vai injetar via Dom o css que ele interpretou
+        }]
+    }
 
 }
+// pasta assets é usada para recursos estaticos
