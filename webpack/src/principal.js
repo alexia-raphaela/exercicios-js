0de64f28fcad5891735a5c1bb20ee2b88372e2bf
Alexia Raphaela <alexiaraphaela@gmail.com>
2021-08-12 11:23:14 -0300

    configuração de ambiente para interpretar sass

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -2,7 +2,7 @@
 import Pessoa from './pessoa'
 // const Pessoa = require('./pessoa')
 import './modulos/moduloA'
-import './assets/css/estilo.css'
+import './assets'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -21,14 +21,16 @@ module.exports = {
         // regras, onde são colocados todos os loders
         rules: [{
             //primeiro loder, responsavel por ler arquivos com a extensão .css
-            test: /\.css$/,
+            // posso formar a partir dessa expressao, uma pesquisa para achar arquivos com extensoes ss
+            test: /\.s?[ac]ss$/,
             // verificar se o arquivo foi lido ou não
             use: [
                 // plugin que extrai para fora o css, para um arquivo externo
                 MiniCssExtractPlugin.loader,
                 // style-loader ira criar uma tag style via dom
                // 'style-loader', // Adciona CSS a DOM injetando a tag <style>
-                'css-loader' // interpreta @import, url()...
+                'css-loader', // interpreta @import, url()...
+                'sass-loader',
             ]
             // ele vai injetar via Dom o css que ele interpretou
         }]
