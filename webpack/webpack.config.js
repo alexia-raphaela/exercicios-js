147e20d6cef408fec31a9b02cc8cb6d75b553ed5
Alexia Raphaela <alexiaraphaela@gmail.com>
2021-08-13 09:44:31 -0300

    instalando dependencias para arquivos com extensoes sass serem minificados

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,28 @@
+// se estiver no modoDEv significa que a variavel process sera diferente de
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const oOptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    // modo de desenvolvimento 
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     // ponto de entrada
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         // pasta destino para salvar os arquivos gerados
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCssAssetsPlugin({})
+        ]
     },
     plugins: [
         // função construtora sendo instanciada, passando objetos como parametros
